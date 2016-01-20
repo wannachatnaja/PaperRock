b81d3980c667fc587b3bd5134e539ea09594e02e
CUPCAKE <red.delete2@gmail.com>
2016-01-20 11:26:27 +0700

    เรียนรู้การใช้ Log.d

--- app/src/main/java/kru/poompunk/wannachat/gamepaperrock/MainActivity.java
+++ app/src/main/java/kru/poompunk/wannachat/gamepaperrock/MainActivity.java
@@ -2,6 +2,7 @@ package kru.poompunk.wannachat.gamepaperrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -59,7 +60,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void changeplay(int intNumber) {
 
-    }
+        Log.d("test", "ค่าที่รับได้ = " + intNumber);
+
+    }// changplay
 
     private void bindWidget() {
         paperImageView = (ImageView) findViewById(R.id.imvPaper);
