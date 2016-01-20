f09c8451fef05c64340fc142cfc9f9267d424561
CUPCAKE <red.delete2@gmail.com>
2016-01-20 10:45:35 +0700

    bindwidget แล้ว

--- app/src/main/java/kru/poompunk/wannachat/gamepaperrock/MainActivity.java
+++ app/src/main/java/kru/poompunk/wannachat/gamepaperrock/MainActivity.java
@@ -2,12 +2,33 @@ package kru.poompunk.wannachat.gamepaperrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit ประกาศตัวแปร
+    private ImageView scissorsImageView, rockImageView, paperImageView, playImageView, androidImageView;
+    private TextView showTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bind widget
+        bindWidget();
+
+    } // main method
+
+    private void bindWidget() {
+        paperImageView = (ImageView) findViewById(R.id.imvPaper);
+        scissorsImageView = (ImageView) findViewById(R.id.imvScissors);
+        rockImageView = (ImageView) findViewById(R.id.imvRock);
+        playImageView = (ImageView) findViewById(R.id.imvPlayer);
+        androidImageView = (ImageView) findViewById(R.id.imvAndroid);
+        showTextView = (TextView) findViewById(R.id.txtShow);
     }
-}
+
+}   // main class
